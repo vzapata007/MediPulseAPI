1cbfa9248c5fe80a2513fbaee5357c286f378d82
Victor Zapata <victor.zapata@msn.com>
2024-06-28 22:08:15 +1200

    Add EMedicine database schema

---
+++ BD_schema/EMedicine_BD.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE EMedicine;
+USE Emedicine;
+
+CREATE TABLE Users (
+    ID INT IDENTITY (1,1) PRIMARY KEY, 
+    FirstName Varchar(100), 
+    LastName Varchar(100), 
+    Password varchar(100),
+    Email varchar(100), 
+    Fund Decimal(18,2), 
+    Type varchar(100), 
+    Status INT, 
+    CreatedOn Datetime
+);
+
+CREATE TABLE Medicines(
+    ID INT IDENTITY (1,1) PRIMARY KEY, 
+    Name VARCHAR(100), 
+    Manufacturer VARCHAR(100), 
+    UnitPrice DECIMAL(18,2),
+    Discount DECIMAL(18,2), 
+    Quantity INT, 
+    ExpDate Datetime, 
+    ImageUrl VARCHAR(100), 
+    Status INT
+);
+
+CREATE TABLE Cart(
+    ID INT IDENTITY (1,1) PRIMARY KEY, 
+    UserId INT, 
+    MedicineId INT, 
+    UnitPrice DECIMAL(18,2), 
+    Discount DECIMAL(18,2), 
+    Quantity INT, 
+    TotalPrice DECIMAL(18,2)
+);
+
+CREATE TABLE Orders(
+    ID INT IDENTITY (1,1) PRIMARY KEY, 
+    UserId INT, 
+    OrderNo VARCHAR(100), 
+    OrderTotal DECIMAL(18,2), 
+    OrderStatus VARCHAR(100)
+);
+
+CREATE TABLE OrderItems(
+    ID INT IDENTITY (1,1) PRIMARY KEY, 
+    UserId INT, 
+    OrderId INT, 
+    MedicineId INT, 
+    UnitPrice Decimal(18,2), 
+    Discount Decimal(18,2),
+    Quantity INT, 
+    TotalPrice Decimal(18,2)
+);
